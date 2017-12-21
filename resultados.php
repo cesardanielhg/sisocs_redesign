fe322933cf453c49afb9f271ea5551c72da8c5d8
cesardanielhg <cesardanielhg@gmail.com>
2017-12-21 00:39:26 -0600

    Archivo de resultados

---
+++ resultados.php
@@ -0,0 +1,193 @@
+<?php include 'header.php'; ?>
+
+		</section>
+		
+		<!--================================SERVICES SECTION==========================================-->
+
+		
+		<section class="categories-section padding-top-10 padding-bottom-10">
+			<div class="container"><!-- section container -->
+				
+				<div class="section-title-wrap margin-bottom-10"><!-- section title -->
+					<h4>Búsqueda en Seccciones Destacadas</h4>
+					<div class="title-divider">
+						<div class="line"></div>
+						<i class="fa fa-cog"></i>
+						<div class="line"></div>
+					</div>
+				</div><!-- section title end -->
+
+			</div>
+		</section>
+
+
+
+				
+		<!--================================SOCIAL CAROUSEL SECTION==========================================-->
+		
+		<section class="social-section padding-bottom-20">
+			<div class="container-fluid"><!-- section container -->
+				<div class="social-wrap style-1">
+					
+					<ul class="social-slider clearfix">
+						
+						<li class="item">
+							<a class="bggray black shadow-1 padding-bottom-10" href="#"><i class="fa fa-road fa-2"></i>
+								<h6>Red Víal</h6>
+							</a>
+						</li><!-- .ITEM -->
+
+						<li class="item">
+							<a class="bggray black shadow-1 padding-bottom-10" href="#"><i class="fa fa-building-o fa-2"></i>
+								<h6>Edificios Gubernamentales</h6>
+							</a>
+						</li><!-- .ITEM -->
+
+						<li class="item">
+							<a class="bggray black shadow-1 padding-bottom-10" href="#"><i class="fa fa-ship fa-2"></i>
+								<h6>Puertos</h6>
+							</a>
+						</li><!-- .ITEM -->
+
+
+						<li class="item">
+							<a class="bggray black shadow-1 padding-bottom-10" href="#"><i class="fa fa-plane fa-2"></i>
+								<h6>Aeropuertos</h6>
+							</a>
+						</li><!-- .ITEM -->
+
+						<li class="item">
+							<a class="bggray black shadow-1 padding-bottom-10" href="#"><i class="fa fa-book fa-2"></i>
+								<h6>Escuelas</h6>
+							</a>
+						</li><!-- .ITEM -->
+
+						<li class="item">
+							<a class="bggray black shadow-1 padding-bottom-10" href="#"><i class="fa fa-medkit fa-2"></i>
+								<h6>Centros de Salud</h6>
+							</a>
+						</li><!-- .ITEM -->
+
+						<li class="item">
+							<a class="bggray black shadow-1 padding-bottom-10" href="#"><i class="fa fa-lightbulb-o fa-2"></i>
+								<h6>Energía</h6>
+							</a>
+						</li><!-- .ITEM -->
+
+						<li class="item">
+							<a class="bggray black shadow-1 padding-bottom-10" href="#"><i class="fa fa-tint fa-2"></i>
+								<h6>Agua</h6>
+							</a>
+						</li><!-- .ITEM -->
+
+
+						<li class="item">
+							<a class="bggray black shadow-1 padding-bottom-10" href="#"><i class="fa fa-signal fa-2"></i>
+								<h6>Comunicaciones</h6>
+							</a>
+						</li><!-- .ITEM -->
+
+						<li class="item">
+							<a class="bggray black shadow-1 padding-bottom-10" href="#"><i class="fa fa-puzzle-piece fa-2"></i>
+								<h6>Areas Recreativas</h6>
+							</a>
+						</li><!-- .ITEM -->
+
+						<div data-background-icon='&#xf086;'></div>
+
+
+
+
+
+
+					</ul>
+				</div>
+			</div><!-- container end -->
+		</section>
+
+		
+				
+
+		<!--================================FUNFACTS COUNTER SECTION==========================================-->
+		
+		<section id="funfact" class=" padding-top-50 padding-bottom-30" >
+			<div class="container">
+				<div class="row padding-bottom-20" id="funfact-1">
+					<div class="col-xs-12 col-sm-6 col-md-4 col-lg-4 margin-bottom-30 text-center clearfix">
+						<div class="funfact-1 color-1  clearfix">
+							<div class="fun-wrap">
+								<div class="count2">791</div>
+								<div class="funfact-divider"></div>
+								<div class="funfact"><p> Proyectos Publicados</p></div>
+							</div>
+						</div>
+					</div><!-- /.col-md-3 col -->
+					
+					<div class="col-xs-12 col-sm-6 col-md-4 col-lg-4 margin-bottom-30 text-center clearfix">
+						<div class="funfact-1 color-1  clearfix">
+							<div class="fun-wrap">
+								<div class="count2">4</div>
+								<div class="funfact-divider"></div>
+								<div class="funfact"><p> Entidades Divulgando Información</p></div>
+							</div>
+						</div>
+					</div><!-- /.col-md-3 col -->  				
+					
+					<div class="col-xs-12 col-sm-6 col-md-4 col-lg-4 margin-bottom-30 text-center clearfix">
+						<div class="funfact-1 color-1  clearfix">
+							<div class="fun-wrap">
+								<div class="count2"> L 44,989,000</div>
+								<div class="funfact-divider"></div>
+								<div class="funfact"><p> Total de Inversión</p></div>
+							</div>
+						</div>
+					</div><!-- /.col-md-3 col -->
+					
+				</div><!-- /#funfact-1 -->
+			</div><!-- container end -->
+		</section>
+		
+
+
+
+		<!--================================ PARTNER SECTION ==========================================-->
+		
+		<section class="partner-section padding-top-40 padding-bottom-20">
+			<div class="container"><!-- section container -->
+				<div class="section-title-wrap margin-bottom-20"><!-- section title -->
+					<h4>entidades de adquisición</h4>
+					<div class="title-divider">
+						<div class="line"></div>
+						<i class="fa fa-cog"></i>
+						<div class="line"></div>
+					</div>
+				</div><!-- section title end -->
+				
+				<div class="row partner-wrap style-1 clearfix">
+					<div class="col-xs-12 col-sm-3 col-md-3 col-lg-3 margin-bottom-30"><!-- .partner -->
+						<div class="partner shadow-1 clearfix">
+							<a href="#"><img src="images/Entidad-fondo.jpg" alt="partner"/></a>
+						</div>
+					</div><!-- partner end -->
+					<div class="col-xs-12 col-sm-3 col-md-3 col-lg-3 margin-bottom-30"><!-- .partner -->
+						<div class="partner shadow-1 clearfix">
+							<a href="#"><img src="images/Entidad-invest.jpg" alt="partner"/></a>
+						</div>
+					</div><!-- partner end -->
+					<div class="col-xs-12 col-sm-3 col-md-3 col-lg-3 margin-bottom-30"><!-- .partner -->
+						<div class="partner shadow-1 clearfix">
+							<a href="#"><img src="images/Entidad-hondutel.jpg" alt="partner"/></a>
+						</div>
+					</div><!-- partner end -->
+					<div class="col-xs-12 col-sm-3 col-md-3 col-lg-3 margin-bottom-30"><!-- .partner -->
+						<div class="partner shadow-1 clearfix">
+							<a href="#"><img src="images/Entidad-enp.jpg" alt="partner"/></a>
+						</div>
+					</div><!-- partner end -->
+				</div><!-- .row partner end -->
+			</div><!-- container end -->
+		</section>
+		
+		
+<?php include 'footer.php'; ?>
+		
