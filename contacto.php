24fdcf5fb8e998c52852c3a6da1246f623015aca
cesardanielhg <cesardanielhg@gmail.com>
2018-01-11 14:00:21 -0600

    Changes on contact form

--- contacto.php
+++ contacto.php
@@ -18,20 +18,25 @@
 								
 								<div class="entry-content">
 									<div class="entry-title">
-										<h4>Take me out of country</h4>
+										<h4>Déjenos sus dudas o consultas</h4>
 									</div>
+
+									
+									
+									<!-- 
 									<div class="entry-metas">
 										<ul>
 											<li><i class="fa fa-user green-1"></i>Posted By:<a href="#"> Admin</a></li>
 											<li><i class="fa fa-clock-o blue-1"></i><a href="#"> March 16, 2015</a></li>
 											<li><i class="fa fa-comment yallow-1"></i><a href="#">4 Comments</a></li>
 										</ul>
-									</div>
+									</div> 
+									-->
+
+
 									<div class="entry-disc">
 										<p>
-											Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex consequat Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-											<br>
-											Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex consequat Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+											Llene el siguiente formulario para que nos deje saber sus dudad o consultas en relación a nuestro sistema o los datos que manejamos.
 										</p>
 									</div>
 								</div>
@@ -39,27 +44,27 @@
 							<div class="comments-wrap margin-top-20">
 								
 								<div class="comments-main-title padding-top-30">
-									<h5 class="tx-left">Leave a comment</h5>
+									<h5 class="tx-left">Contáctenos</h5>
 								</div>
 								<div class="contact-form-wrap margin-top-30"><!--.contact-form-wrap -->
 									<form class="row contact-form"><!-- .row .contact-form -->
 										<div class="col-xs-12 col-sm-6 col-md-6 col-lg-6"><!-- form-field -->
-											<input class="input-field" type="text" placeholder="FIRST NAME" name="fname">
+											<input class="input-field" type="text" placeholder="NOMBRE" name="fname">
 										</div><!-- form-field  end-->
 										<div class="col-xs-12 col-sm-6 col-md-6 col-lg-6"><!-- form-field -->
-											<input class="input-field" type="text" placeholder="LAST NAME" name="lname">
+											<input class="input-field" type="text" placeholder="APELLIDO" name="lname">
 										</div><!-- form-field  end-->
 										<div class="col-xs-12 col-sm-6 col-md-6 col-lg-6"><!-- form-field -->
-											<input class="input-field" type="text" placeholder="EMAIL ADDRESS" name="email">
+											<input class="input-field" type="text" placeholder="CORREO ELECTRONICO" name="email">
 										</div><!-- form-field  end-->
 										<div class="col-xs-12 col-sm-6 col-md-6 col-lg-6"><!-- form-field -->
 											<input class="input-field" type="text" placeholder="WEBSITE" name="website">
 										</div><!-- form-field  end-->
 										<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12"><!-- form-field -->
-											<textarea class="input-field" placeholder="MESSAGE" name="message"></textarea>
+											<textarea class="input-field" placeholder="MENSAGE" name="message"></textarea>
 										</div><!-- form-field  end-->
 										<div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 pull-right"><!-- form-btn -->
-											<input class="contact-btn" type="submit" value="SUBMIT MESSAGE">
+											<input class="contact-btn" type="submit" value="ENVIAR INFORMACION">
 										</div><!-- form-btn  end-->
 									</form><!-- .row .contact-form end -->
 								</div><!--.contact-form-wrap end -->
